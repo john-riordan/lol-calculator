a457dcb882209aa5eca111878519303595f75652
John Riordan <iam.john.riordan@gmail.com>
2022-06-25 23:48:16 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+
+	async function loadData() {
+		const data = await fetch('/data');
+		const json = await data.json();
+
+		return json.champions;
+	}
+
+	/** @type {Object} */
+	let data = {};
+
+	onMount(async () => {
+		data = await loadData();
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
